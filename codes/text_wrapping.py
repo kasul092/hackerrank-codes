7f03de6e1a857474bc21d2ecf45a1b8b2b6ff742
kasul <sumitpujari199723@gmail.com>
2022-03-06 15:08:57 +0530

    break string into groups of 4 and print it in new line

---
+++ codes/text_wrapping.py
@@ -0,0 +1,15 @@
+a_string = "abcdefghijklmnopqrstuvwxyz"
+# s = ""
+n = 4
+# for index in range(0, len(a_string), n):
+#     s = a_string[index : index + n]
+#     print(s)
+
+
+import textwrap
+
+def wrap(a_string, n):
+    a = textwrap.fill(a_string, n)
+    print(a)
+    return a
+wrap("abcdefghijklmnopqrstuvwxyz", 4)
