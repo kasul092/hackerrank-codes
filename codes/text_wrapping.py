906edc4540b9b70cbab982c700ccd8e61d61e3d8
kasul <sumitpujari199723@gmail.com>
2022-03-06 17:19:59 +0530

    break the string into 4 and print it in new line

--- codes/text_wrapping.py
+++ codes/text_wrapping.py
@@ -1,6 +1,6 @@
-a_string = "abcdefghijklmnopqrstuvwxyz"
+# a_string = "abcdefghijklmnopqrstuvwxyz"
 # s = ""
-n = 4
+# n = 4
 # for index in range(0, len(a_string), n):
 #     s = a_string[index : index + n]
 #     print(s)
